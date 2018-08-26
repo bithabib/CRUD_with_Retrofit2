58fe1945737f9e73a7268ba8a856aa293970a432
Md Habibur Rahman <habib.usa2014@gmail.com>
2018-08-26 16:23:25 +0600

    Login complete

--- app/src/main/java/com/employeeinformation/io/respay/employeeinformation/INetWorkService.java
+++ app/src/main/java/com/employeeinformation/io/respay/employeeinformation/INetWorkService.java
@@ -13,6 +13,9 @@ public interface INetWorkService {
     @FormUrlEncoded
     @POST("/webapp/insert.php")
     Call<Employee> insertData(@Field("name") String name, @Field("type") String type ,@Field("id") String id , @Field("address") String address , @Field("date_of_joining") String date_of_joining , @Field("password") String password);
+    @FormUrlEncoded
+    @POST("/webapp/login.php")
+    Call<Employee> login(@Field("id") String id , @Field("password") String password);
 
     @GET("/webapp/get.php")
     Call<List<Employee>>getData();
--- app/src/main/java/com/employeeinformation/io/respay/employeeinformation/LoginAndRegistrationFragment/LogIn.java
+++ app/src/main/java/com/employeeinformation/io/respay/employeeinformation/LoginAndRegistrationFragment/LogIn.java
@@ -1,18 +1,28 @@
 package com.employeeinformation.io.respay.employeeinformation.LoginAndRegistrationFragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.employeeinformation.io.respay.employeeinformation.All_Activity.CrudOperation;
 import com.employeeinformation.io.respay.employeeinformation.All_Activity.LoginAndRegistration;
+import com.employeeinformation.io.respay.employeeinformation.ApiClient;
+import com.employeeinformation.io.respay.employeeinformation.Employee;
+import com.employeeinformation.io.respay.employeeinformation.INetWorkService;
 import com.employeeinformation.io.respay.employeeinformation.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +32,10 @@ public class LogIn extends Fragment {
     TextView linkToRegister;
     @BindView(R.id.btnLogin)
     Button logIn;
+    @BindView(R.id.id)
+    TextView id;
+    @BindView(R.id.password)
+    TextView password;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -37,7 +51,29 @@ public class LogIn extends Fragment {
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                INetWorkService service = ApiClient.getClient().create(INetWorkService.class);
+                Employee employee = new Employee();
+                employee.setId(id.getText().toString());
+                employee.setPassword(password.getText().toString());
+                Call<Employee> call = service.login(employee.getId(),employee.getPassword());
+                call.enqueue(new Callback<Employee>() {
+                    @Override
+                    public void onResponse(Call<Employee> call, Response<Employee> response) {
+                        if(response.body().getStatus()==1){
+                            Intent intent = new Intent(getActivity(), CrudOperation.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(getActivity(), "Wrong password or id", Toast.LENGTH_LONG).show();
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<Employee> call, Throwable t) {
+                        Log.i("Hello",""+t);
+                        Toast.makeText(getActivity(), "Throwable"+t, Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
         return viewOfLogin;
